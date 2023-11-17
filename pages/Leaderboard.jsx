bd570647751a9089399ba1b209259a1b4f4f8e4c
FirosyanAmmarSopyan <70552593+FirosyanAmmarSopyan@users.noreply.github.com>
2023-11-17 14:27:58 +0700

    fixed bottom tab and leaderboard

--- pages/Leaderboard.jsx
+++ pages/Leaderboard.jsx
@@ -5,10 +5,83 @@ import {
   StatusBar,
   ImageBackground,
   ScrollView,
+  FlatList,
 } from "react-native";
 import Cardlead from "../components/CardLeaderboard";
 
 export default function Leaderboard() {
+  const DATA = [
+    {
+      id: "1",
+      title : "res"
+    },
+    {
+      id: "2",
+      title : "res"
+    },
+    {
+      id: "3",
+      title : "res"
+    },
+    {
+      id: "4",
+      title : "res"
+    },
+    {
+      id: "5",
+      title : "res"
+    },
+    {
+      id: "6",
+      title : "res"
+    },
+    {
+      id: "7",
+      title : "res"
+    },
+    {
+      id: "8",
+      title : "res"
+    },
+    {
+      id: "9",
+      title : "res"
+    },
+    {
+      id: "10",
+      title : "res"
+    },
+    {
+      id: "11",
+      title : "res"
+    },
+    {
+      id: "12",
+      title : "res"
+    },
+    {
+      id: "13",
+      title : "res"
+    },
+    {
+      id: "14",
+      title : "res"
+    },
+    {
+      id: "15",
+      title : "res"
+    },
+    {
+      id: "16",
+      title : "res"
+    },
+    {
+      id: "17",
+      title : "res"
+    },
+
+  ];
+
   return (
     <ImageBackground
       source={require("../assets/bg-leaderboard.jpg")}
@@ -18,19 +91,16 @@ export default function Leaderboard() {
         <View style={styles.titleContainer}>
           <Text style={styles.title}>LEADERBOARD</Text>
         </View>
-        <ScrollView style={styles.containerList}  showsVerticalScrollIndicator={false}>
+        {/* <ScrollView style={styles.containerList}  showsVerticalScrollIndicator={false}>
           <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-          <Cardlead />
-        </ScrollView>
+        </ScrollView> */}
+        <FlatList
+        style={styles.containerList}
+          data={DATA}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item  , index}) => <Cardlead title={item.title} index={index} />}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </ImageBackground>
   );
