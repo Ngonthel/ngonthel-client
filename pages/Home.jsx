e57683258d73abc473cf3d3ed93a73004a80418a
oktaviaqa <oktaviaq.a@gmail.com>
2023-11-17 17:11:10 +0700

    HomePage

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,16 @@
-import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 function HomePage() {
   return (
     <SafeAreaView style={styles.container}>
-      <View className="flex flex-row ml-5 mr-5">
+      <View className="flex flex-row ml-5 mr-5 mt-5">
         <View className="flex-1 w-64 items-left justify-center">
           <Text className="text-lg text-[#696e74]">
             Hi, <Text className="font-bold text-[#293038]">[ Username ]</Text>
@@ -22,35 +30,65 @@ function HomePage() {
           <Text className="font-extrabold text-[#293038]">Zone</Text>
         </Text>
       </View>
-      <View className="w-15 h-25 mt-7 ml-5 mr-5">
-        <Text className="text-lg mb-3 font-bold text-[#293038] ">
+      <View className="w-15 h-25 mt-7 ml-5 mr-5 ">
+        <Image
+          className=" w-96 h-44"
+          source={require("../assets/ngontel2.png")}
+        />
+        {/* RECENT HISTORY */}
+        <Text className="text-lg mb-3 font-bold text-[#293038]  ">
           Recent History
         </Text>
-        <View className="rounded-lg overflow-hidden drop-shadow-2xl border-2 border-gray-500  ">
-          {/* <Text className="text-xl font-bold text-gray-800">Distance Total</Text> */}
-          <View className="flex flex-row gap-x-20 ">
-            <View>
-              <Text className="text-lg font-bold text-gray-800">Range</Text>
+        <View className="flex flex-row gap-x-5 items-center justify-center ">
+          <View className="rounded-lg bg-white shadow-sm shadow-slate-300 p-2">
+            <View className="mr-5 ml-5">
+              <Text className="text-lg font-bold text-gray-800">Distance</Text>
               <Text className="text-sm text-center mt-1">50mil</Text>
             </View>
-            <View>
+          </View>
+          <View className="rounded-lg bg-white shadow-sm shadow-slate-300 p-2">
+            <View className="mr-5 ml-5">
               <Text className="text-lg font-bold text-gray-800">Speed</Text>
               <Text className="text-sm text-center mt-1">75km/H</Text>
             </View>
-            <View>
-              <Text className="text-lg font-bold text-gray-800">Power</Text>
+          </View>
+          <View className="rounded-lg bg-white shadow-sm shadow-slate-300 p-2">
+            <View className="mr-5 ml-5">
+              <Text className="text-lg font-bold text-gray-800">Time</Text>
               <Text className="text-sm text-center mt-1">387wH</Text>
             </View>
-            {/* <Text className="text-gray-600 mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Text> */}
-            <View className="mt-4"></View>
           </View>
         </View>
-      </View>
-      <View>
-        
+        <View className="flex flex-row ml-5 mr-3 mt-3">
+          <View className="flex-1 w-64 items-left justify-center"></View>
+          <Text className="text-md font-medium text-[#FFC329]">See all</Text>
+        </View>
+        {/* GO CYCLING */}
+        <Text className="text-lg mb-3 font-bold text-[#293038]">
+          Go Cycling
+        </Text>
+        <View className="rounded-lg h-32 bg-white shadow-sm shadow-slate-300 p-1">
+          <View className="flex flex-row p-1">
+            <Image
+              className="w-32 h-28"
+              source={require("../assets/map.png")}
+            />
+            <View>
+            <Text className="text-sm mt-1 w-52 ml-2 p-1">
+              Find your location, and unclock a world of cycling adventures with
+              Gowez!
+            </Text>
+            <View className="items-center justify-end mt-5"
+          >
+            <TouchableOpacity className="rounded-full w-20 h-6 p-1 bg-[#FFC329] items-center">
+              <Text className="text-md items-center justify-center"> Go </Text>
+            </TouchableOpacity>
+          </View>
+            </View>
+            
+          </View>
+          
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -62,9 +100,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     marginRight: 7,
     marginLeft: 7,
-    marginTop: 45,
-    //   alignItems: "center",
-    //   justifyContent: "center",
   },
 });
 
