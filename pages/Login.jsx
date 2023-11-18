d9727358b6f2518dcdf69d8fc4e0804fbea1050a
FirosyanAmmarSopyan <70552593+FirosyanAmmarSopyan@users.noreply.github.com>
2023-11-18 11:39:21 +0700

    create page login

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,30 +1,95 @@
-import { View, Text, StyleSheet, StatusBar, TextInput } from "react-native";
-import Logo from "../assets/logo-gowez.svg";
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
+import Logo from '../assets/logoo.svg';
+
 export default function Login() {
   return (
-    <View style={styles.AndroidSafeArea}>
-      <View style={{ alignItems : 'center'}}>
-      <Logo width={250} />
-      <Text style={{fontSize : 26 , fontWeight : 'bold'}}>Login</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.AndroidSafeArea}
+    >
+      <View style={styles.logoContainer}>
+        <Logo  width={250} />
       </View>
+        <Text style={{ fontSize: 26, fontWeight: 'bold' , alignSelf : "center" }}>Login</Text>
       <View style={styles.containerForm}>
-        <TextInput  />
+        <View>
+          <Text style={{ marginVertical: 10, fontSize: 18, fontWeight: '500' }}>Email:</Text>
+          <TextInput
+            style={{
+              backgroundColor: 'white',
+              height: 40,
+              fontSize: 18,
+              borderRadius: 20,
+              paddingHorizontal: 5,
+            }}
+            placeholder="Enter Email ...."
+          />
+        </View>
+        <View>
+          <Text style={{ marginVertical: 10, fontSize: 18, fontWeight: '500' }}>Password:</Text>
+          <TextInput
+            style={{
+              backgroundColor: 'white',
+              height: 40,
+              fontSize: 18,
+              borderRadius: 20,
+              paddingHorizontal: 5,
+            }}
+            placeholder="Enter Password ...."
+            secureTextEntry={true}
+          />
+        </View>
       </View>
-    </View>
+      <View>
+      <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Login</Text>
+    </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     paddingHorizontal: 14,
-    backgroundColor : 'white'
+    backgroundColor: 'white',
+    marginTop: -20
   },
-  containerForm : {
-    padding : 15,
-    backgroundColor : '#696e74',
-    marginTop : 20,
-    borderRadius : 10
-  }
+  logoContainer: {
+    alignItems: 'center',
+    width: 'auto',
+  },
+  containerForm: {
+    padding: 15,
+    backgroundColor: '#F4F4F4',
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: '#293038',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginVertical : 20,
+    alignSelf : 'center',
+    width :200
+  },
+  buttonText: {
+    color: '#FFC329',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
