ec975b45eb95df76dbfcbd145c862e84e35c017c
FirosyanAmmarSopyan <70552593+FirosyanAmmarSopyan@users.noreply.github.com>
2023-11-20 15:47:54 +0700

    fix register and login

--- navigator/MainStack.jsx
+++ navigator/MainStack.jsx
@@ -2,12 +2,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../pages/Home"
 import Leaderboard from "../pages/Leaderboard";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 export default function MainStack(){
     const Stack = createNativeStackNavigator()
     return(
         <Stack.Navigator screenOptions={{headerShown : false }}>
             <Stack.Screen  name="home" component={Home}  />
-            <Stack.Screen name="login" component={Login} />
         </Stack.Navigator>
     )
     
